881ca3353a258872b0f8856490a93a97c87d0fe4
Louis Monday <xenomedesign@gmail.com>
2014-06-13 15:11:30 -0700

    Update routes.js

--- web/app/main/routes.js
+++ web/app/main/routes.js
@@ -1,15 +1,5 @@
-App.ApplicationRoute = Ember.Route.extend(Ember.SimpleAuth.ApplicationRouteMixin, {
+App.ApplicationRoute = Ember.Route.extend( {
     actions: {
-        error: function(error, transition) {
-            if (error.status === 401) {
-                console.log("session failed");
 
-                this.session.set('isAuthenticated', false);
-
-                transition.abort();
-                this.set('session.attemptedTransition', transition);
-                transition.send('authenticateSession');
-            }
-        }
     }
 });
